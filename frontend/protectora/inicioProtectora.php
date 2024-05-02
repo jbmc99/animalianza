53b99aaca54e2139c6511b4025af390d97fbc3a3
jbmc99 <127203600+jbmc99@users.noreply.github.com>
2024-05-02 20:29:35 +0200

    a

--- frontend/protectora/inicioProtectora.php
+++ frontend/protectora/inicioProtectora.php
@@ -41,59 +41,74 @@
   </nav>
 
 <div class="container mt-5">
-    <h1 class="text-center mb-4">¡Bienvenid@!</h1>
-    <h3 class="text-center mb-4">¿Qué deseas hacer hoy?</h3>
-    <div class="row">
-      <!-- Card: Gestionar Animales -->
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 text-center bg-transparent border-2">
-          <div class="card-body">
-            <h5 class="card-title">Gestionar Animales</h5>
-            <p class="card-text">Añadir un animal nuevo o eliminar uno existente.</p>
-          </div>
-          <div class="card-footer">
-            <a href="../protectora/gestionAnimales.php" class="btn btn-success">Ir a la gestión</a>
-          </div>
+  <h1 class="text-center mb-4">¡Bienvenid@!</h1>
+  <h3 class="text-center mb-4">¿Qué deseas hacer hoy?</h3>
+  <div class="row justify-content-center">
+    <!-- Card: Gestionar Animales -->
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card h-100 text-center bg-transparent border-2">
+        <div class="card-body">
+          <h5 class="card-title">Gestionar Animales</h5>
+          <p class="card-text">Añadir un animal nuevo o eliminar uno existente.</p>
+        </div>
+        <div class="card-footer">
+          <a href="../protectora/gestionAnimales.php" class="btn btn-success">Ir a la gestión</a>
         </div>
       </div>
-      <!-- Card: Adopciones -->
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 text-center bg-transparent border-2">
-          <div class="card-body">
-            <h5 class="card-title">Adopciones</h5>
-            <p class="card-text">Ver y gestionar procesos de adopción de animales.</p>
-          </div>
-          <div class="card-footer">
-            <a href="../protectora/gestionAdopciones.php" class="btn btn-success">Ir a adopciones</a>
-          </div>
+    </div>
+    <!-- Card: Adopciones -->
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card h-100 text-center bg-transparent border-2">
+        <div class="card-body">
+          <h5 class="card-title">Adopciones</h5>
+          <p class="card-text">Ver y gestionar procesos de adopción de animales.</p>
+        </div>
+        <div class="card-footer">
+          <a href="../protectora/gestionAdopciones.php" class="btn btn-success">Ir a adopciones</a>
         </div>
       </div>
-      <!-- Card: Casa de Acogida -->
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 text-center bg-transparent border-2">
-          <div class="card-body">
-            <h5 class="card-title">Casa de Acogida</h5>
-            <p class="card-text">Administrar animales en casa de acogida.</p>
-          </div>
-          <div class="card-footer">
-            <a href="../protectora/gestionCasaAcogida.php" class="btn btn-success">Ir a casa de acogida</a>
-          </div>
+    </div>
+    <!-- Card: Casa de Acogida -->
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card h-100 text-center bg-transparent border-2">
+        <div class="card-body">
+          <h5 class="card-title">Casa de Acogida</h5>
+          <p class="card-text">Administrar animales en casa de acogida.</p>
         </div>
-      </div>
-      <!-- Card: Eventos -->
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 text-center bg-transparent border-2">
-          <div class="card-body">
-            <h5 class="card-title">Eventos</h5>
-            <p class="card-text">Agregar o finalizar eventos relacionados con la protectora.</p>
-          </div>
-          <div class="card-footer">
-            <a href="../protectora/gestionEventos.php" class="btn btn-success">Ir a eventos</a>
-          </div>
+        <div class="card-footer">
+          <a href="../protectora/gestionCasaAcogida.php" class="btn btn-success">Ir a casa de acogida</a>
         </div>
       </div>
     </div>
   </div>
+  <div class="row justify-content-center">
+    <!-- Card: Eventos -->
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card h-100 text-center bg-transparent border-2">
+        <div class="card-body">
+          <h5 class="card-title">Eventos</h5>
+          <p class="card-text">Agregar o finalizar eventos relacionados con la protectora.</p>
+        </div>
+        <div class="card-footer">
+          <a href="../protectora/gestionEventos.php" class="btn btn-success">Ir a eventos</a>
+        </div>
+      </div>
+    </div>
+    <!-- Card: Productos -->
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card h-100 text-center bg-transparent border-2">
+        <div class="card-body">
+          <h5 class="card-title">Productos</h5>
+          <p class="card-text">Ver y gestionar tus productos.</p>
+        </div>
+        <div class="card-footer">
+          <a href="../protectora/gestionProductos.php" class="btn btn-success">Ir a productos</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
   <!--FOOTER-->
 <footer class="text-center text-lg-start bg-body-tertiary text-muted mt-5" id="footer">
